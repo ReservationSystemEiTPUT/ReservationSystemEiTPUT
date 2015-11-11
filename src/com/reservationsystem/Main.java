3b605d2f84ef27328650ea7292f2bbf31aba3dc2
Piotr <Piotrek>
2015-11-11 12:08:42 +0100

    Second commit

--- src/com/reservationsystem/Main.java
+++ src/com/reservationsystem/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("App.fxml"));
-			Scene scene = new Scene(root,400,400);
+			BorderPane root1 = (BorderPane)FXMLLoader.load(getClass().getResource("App.fxml"));
+			Scene scene = new Scene(root1,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
