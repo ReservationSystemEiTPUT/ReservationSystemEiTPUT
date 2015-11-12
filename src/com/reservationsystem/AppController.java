7212f5a88a0af4f41c7316f17a9abd9c51a99112
Piotr <Piotrek>
2015-11-12 20:06:30 +0100

    Dodalem jakies tam okienka, jeszcze łączenia z bazą nie ma :P

--- src/com/reservationsystem/AppController.java
+++ src/com/reservationsystem/AppController.java
@@ -1,5 +1,21 @@
 package com.reservationsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AppController {
+	@FXML 
+	public TextField nazwaUzytkownika;
+	@FXML 
+	public TextField wyswietlacz;
 	
+	public void loginClicked() 
+	{
+	 System.out.println("Helloo");
+	}
+	
+	public void wpisywanie()
+	{
+	wyswietlacz.setText(nazwaUzytkownika.getText());
+	}
 }
